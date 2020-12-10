5e6a611d2650aea2611fa949f260e8d895e10314
Mike U <urciuolim@gmail.com>
2020-12-09 19:22:54 -0500

    Render scripts added

---
+++ render-GPU-Coarse-Grid.sh
@@ -0,0 +1,8 @@
+cd GPU-Coarse-Grid/
+if [[ -f log2.txt ]]
+then
+	./simulator -l
+else
+	echo "log2.txt not detected, please run simulation and ensure that logging file is saved at log2.txt. Running the example script will do."
+fi
+cd ..
